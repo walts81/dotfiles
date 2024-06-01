0b8f4a5fb74099571d79de45b9e115b95e69372b
Xcav8r <xcav8r001@gmail.com>
2024-06-01 13:08:31 -0500

    updated neovim config, zsh, and added tokyonight themes

---
+++ neovim/.config/nvim/init.lua
@@ -0,0 +1,5 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+-- post config should always be last
+require("config.post-config")
---
+++ zsh/.fzf.bash
@@ -0,0 +1,7 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/jwalters/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/home/jwalters/.fzf/bin"
+fi
+
+eval "$(fzf --bash)"
